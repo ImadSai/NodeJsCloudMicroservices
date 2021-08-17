9829b15cd143a7b183e0fa63b118f62f9acd6e73
imad <imad.salki@hotmail.fr>
2021-08-17 16:23:30 +0200

    Add front end

--- auth/src/routes/__test__/signout.test.ts
+++ auth/src/routes/__test__/signout.test.ts
@@ -2,23 +2,11 @@ import request from 'supertest';
 import { app } from '../../app';
 
 it('fails when an email does not exist is supplied', async () => {
-    await request(app)
-        .post('/api/users/signup')
-        .send({
-            email: 'test@test.com',
-            password: 'password'
-        }).expect(201);
-
-    await request(app)
-        .post('/api/users/signin')
-        .send({
-            email: 'test@test.com',
-            password: 'password'
-        })
-        .expect(200);
+    const cookie = await signin();
 
     const response = await request(app)
         .post('/api/users/signout')
+        .set('Cookie', cookie)
         .send({})
         .expect(200);
 
