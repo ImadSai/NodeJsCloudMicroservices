12e625fe44f97b025dca8a1f2bcaa1a3da5a3a75
imad <imad.salki@hotmail.fr>
2021-09-22 03:26:56 +0200

    Update worker

--- client-tcp/src/app.ts
+++ client-tcp/src/app.ts
@@ -1,12 +1,12 @@
 import net from 'net';
 
 const serviceName = "Logging_centralize";
-const PORT = 3000
-const IP = '127.0.0.1'
+const PORT = 9000
+const HOST = 'ticketing.test'
 
 var client = new net.Socket();
 
-client.connect(PORT, IP, function () {
+client.connect(PORT, HOST, function () {
     console.log('Connected');
     client.write('Hello, server! Love, Client.');
 });
